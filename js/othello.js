26b64bf4dc31dd599ded4333b1e8b319660c7def
soshi-matsuo <msoshi1202@gmail.com>
2019-09-01 16:44:24 +0900

    beautify code

--- js/othello.js
+++ js/othello.js
@@ -7,14 +7,14 @@ var iny = 0;
 var turn = 1;
 // array for the stones
 var point = new Array(10);
-for(var i=0; i<10; i++){
+for(var i = 0; i < 10; i++){
     point[i] = [0,0,0,0,0,0,0,0,0,0];
 }
- 
+
 onload = function(){
     var canvas = document.getElementById('canvas');
     var ctx = canvas.getContext('2d');
- 
+
     canvas.addEventListener('click',function(event){
                 mouseX = event.pageX;
                 mouseY = event.pageY;
@@ -25,35 +25,35 @@ onload = function(){
                 false);
     initialize(ctx);
 };
- 
+
 // initialize the board
 function initialize(ctx){
     ctx.beginPath();
-    for(var i = 1; i < 8; i++){
-    ctx.moveTo(0,i*80);
-    ctx.lineTo(640,i*80);
-    ctx.moveTo(i*80,0);
-    ctx.lineTo(i*80,640);
+    for (var i = 1; i < 8; i++) {
+        ctx.moveTo(0,i*80);
+        ctx.lineTo(640,i*80);
+        ctx.moveTo(i*80,0);
+        ctx.lineTo(i*80,640);
     }
 
     ctx.stroke();
- 
+
     point[4][5] = 1;
     point[5][4] = 1;
     point[4][4] = 2;
     point[5][5] = 2;
     drawStones(ctx);
 }
- 
- 
+
+
 // draw stones on the board
 function drawStones(ctx){
-    for(var i=1;i<10;i++){
-        for(var j=1;j<10;j++){
+    for (var i = 1; i < 10; i++) {
+        for (var j = 1; j < 10; j++) {
             var color = '';
-            if(point[i][j] == 1){
+            if (point[i][j] == 1) {
                 color = 'black';
-            }else if(point[i][j] == 2){
+            } else if (point[i][j] == 2) {
                 color = 'white';
             }
             fixStoneColor(ctx,i-1,j-1,color);
@@ -63,6 +63,7 @@ function drawStones(ctx){
 
 function fixStoneColor(ctx,inx,iny,color){
     if (color != 'black' && color != 'white') return;
+
     ctx.beginPath();
     ctx.arc(40+inx*80, 40+iny*80, 35, 0, Math.PI*2, true);
     ctx.strokeStyle = color;
@@ -74,18 +75,18 @@ function fixStoneColor(ctx,inx,iny,color){
 function flipStones(inx,iny){
     // fliped or not
     var flag = false;
-    if(point[inx][iny]!=0)　return;
+    if (point[inx][iny] != 0)　return;
     // 8 directions
-    for(var dx = -1; dx <= 1; dx++){
-        for(var dy = -1; dy <= 1; dy++){
-            if(dx == 0 && dy == 0)　continue;
+    for (var dx = -1; dx <= 1; dx++) {
+        for (var dy = -1; dy <= 1; dy++) {
+            if (dx == 0 && dy == 0)　continue;
             var n = 1;
-            while(point[inx + n*dx][iny + n*dy] == 3 - turn){
+            while (point[inx + n*dx][iny + n*dy] == 3 - turn) {
                 n++;
             }
-            if(n > 1 && point[inx + n*dx][iny + n*dy] == turn){
+            if (n > 1 && point[inx + n*dx][iny + n*dy] == turn) {
                 n = 1;
-                while(point[inx + n*dx][iny + n*dy] == 3 - turn){
+                while (point[inx + n*dx][iny + n*dy] == 3 - turn) {
                     point[inx + n*dx][iny + n*dy] = turn;
                     n++;
                 }
@@ -93,12 +94,12 @@ function flipStones(inx,iny){
             }
         }
     }
-    if(flag){
-    point[inx][iny] = turn;
-    turn = 3 - turn;
+    if (flag) {
+        point[inx][iny] = turn;
+        turn = 3 - turn;
     }
 }
- 
+
 // fix the coordinate to make the left top point of canvas the origin
 function fixCoordinate(canvas){
     rect = canvas.getBoundingClientRect();
