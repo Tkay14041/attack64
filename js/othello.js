7ee19e3b63f3ba8c941920930ea4610f0c8cd0a5
Tkay14041 <takuya@uchida.im>
2019-09-01 17:19:34 +0900

    refactor flip stone

--- js/othello.js
+++ js/othello.js
@@ -4,7 +4,7 @@ var mouseY;
 var inx = 0;
 var iny = 0;
 // turn; black:1 white:2
-var turn = 1;
+var colorOfTurn = 1;
 // array for the stones
 var point = new Array(10);
 for(var i = 0; i < 10; i++){
@@ -75,28 +75,27 @@ function fixStoneColor(ctx,inx,iny,color){
 function flipStones(inx,iny){
     // fliped or not
     var flag = false;
+    var oppositeColor = 3 - colorOfTurn;
     if (point[inx][iny] != 0)　return;
     // 8 directions
     for (var dx = -1; dx <= 1; dx++) {
         for (var dy = -1; dy <= 1; dy++) {
             if (dx == 0 && dy == 0)　continue;
             var n = 1;
-            while (point[inx + n*dx][iny + n*dy] == 3 - turn) {
+            while (point[inx + n * dx][iny + n * dy] == oppositeColor) {
                 n++;
             }
-            if (n > 1 && point[inx + n*dx][iny + n*dy] == turn) {
-                n = 1;
-                while (point[inx + n*dx][iny + n*dy] == 3 - turn) {
-                    point[inx + n*dx][iny + n*dy] = turn;
-                    n++;
+            if (n > 1 && point[inx + n * dx][iny + n * dy] == colorOfTurn) {
+                for (var i = 1; i <= n; i++) {
+                    point[inx + i * dx][iny + i * dy] = colorOfTurn;
                 }
-            flag = true;
+                flag = true;
             }
         }
     }
     if (flag) {
-        point[inx][iny] = turn;
-        turn = 3 - turn;
+        point[inx][iny] = colorOfTurn;
+        colorOfTurn = oppositeColor;
     }
 }
 
