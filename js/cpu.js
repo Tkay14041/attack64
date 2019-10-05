ca27bdfc809deb4ebdfeec739358f1c723f7d732
Uchida, Takuya | Tkay | PAYSD <takuya.uchida@rakuten.com>
2019-10-05 16:28:53 +0900

    cpu power up

--- js/cpu.js
+++ js/cpu.js
@@ -1,11 +1,36 @@
 "use strict";
 
+var scoreBoard = new Array(10);
+scoreBoard[0] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+scoreBoard[1] = [0,        100,  0, 10,  3,  3, 10,  0, 100,        0];
+scoreBoard[2] = [0,          0,  0, 10,  2,  2, 10,  0,   0,        0];
+scoreBoard[3] = [0,         10, 10, 20,  5,  5, 20, 10,  10,        0];
+scoreBoard[4] = [0,          3,  2,  5, 50, 50,  5,  2,   3,        0];
+scoreBoard[5] = [0,          3,  2,  5, 50, 50,  5,  2,   3,        0];
+scoreBoard[6] = [0,         10, 10, 20,  5,  5, 20, 10,  10,        0];
+scoreBoard[7] = [0,          0,  0, 10,  2,  2, 10,  0,   0,        0];
+scoreBoard[8] = [0,        100,  0, 10,  3,  3, 10,  0, 100,        0];
+
+scoreBoard[9] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+
 var CPU = CPU || {
     play: function(ctx) {
         var cpuPotentialCells = Othello.getPotentialCells();
         if (!cpuPotentialCells.length) return;
-        var cpuIdx = Math.floor(Math.random() * cpuPotentialCells.length);
-        var cpuCell = cpuPotentialCells[cpuIdx];
+        // var cpuIdx = Math.floor(Math.random() * cpuPotentialCells.length);
+        var cpuCell = [];
+        var max = 0;
+        for (var cell of cpuPotentialCells) {
+          max = 0;
+          var score = scoreBoard[cell[0]][cell[1]];
+          if (max <= score) {
+            max = score;
+            cpuCell[0] = cell[0];
+            cpuCell[1] = cell[1];
+          }
+        }
         Othello.flipStones(cpuCell[0], cpuCell[1]);
         Common.drawStones(ctx);
     },
